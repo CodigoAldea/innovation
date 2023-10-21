f2a759f4a790ebb562cb0986bec608ef6e4fab47
CodigoAldea <dmdndrd@gmail.com>
2023-10-21 23:00:07 +0530

    new_dhawal_1

--- form/views.py
+++ form/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from .models import Branches,Project
 from django.views.generic import ListView
 from .models import Contact
+from django.core.paginator import Paginator
 
 
-def project_list(request,pk):
-    branch=Branches.objects.get(name=pk)
-    projects=Project.objects.filter(branch=branch)
-    return render(request, 'data.html', {'projects': projects})
+def project_list(request, pk):
+    branch = Branches.objects.get(name=pk)
+    projects = Project.objects.filter(branch=branch)
+    items_per_page = 12  # Adjust as needed
+    paginator = Paginator(projects, items_per_page)
+    page_number = request.GET.get('page')
+
+    # Get the Page object for the current page
+    page = paginator.get_page(page_number)
+    return render(request, 'data.html', {'page': page, 'branch': branch})
 
 def pro_detail(request,pk):
     # branch=Branches.objects.get(name=pk)
